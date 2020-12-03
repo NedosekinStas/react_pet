59936fa6805439112afe4d961aff9d1f0a07e44e
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-12-03 04:46:11 +0300

    add loader, create api jsonPlaceholder...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';     // userEffect хук который подгружаем к получению json
 import BookList from './Book/BookList';
 import Context from './context';
 import AddBook from './Book/AddBook';
+import Loader from './loader';
 
 function App() {
   // Так называемый хук возвращающий массив из двух элементов (состояние, функция для изменения состояния)
   const [books, setBooks] = React.useState([
-    { id: 1, completed: false, title: 'А.С Пушкин. Сказка о царе Салтане' },
-    { id: 2, completed: false, title: 'В.В Набоков. Камера обскура' },
-    { id: 3, completed: false, title: 'Н.В Гоголь. Вечера на хуторе близ Диканьки' }
+    // { id: 1, completed: false, title: 'А.С Пушкин. Сказка о царе Салтане' },
+    // { id: 2, completed: false, title: 'В.В Набоков. Камера обскура' },
+    // { id: 3, completed: false, title: 'Н.В Гоголь. Вечера на хуторе близ Диканьки' }
   ]);
+
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=15')
+      .then(response => response.json())
+      .then(books => {
+        setTimeout(() => {
+          setBooks(books)
+          setLoading(false)
+        }, 2000)
+        
+      })
+  }, [])
 
   function toggleBook(id) { 
     // изменит массив когда вернется и тем самым изменит стэйт
@@ -48,10 +63,14 @@ function App() {
 
         <AddBook onCreate={addBook} />
 
+        {loading && <Loader />}
+
         {books.length ? (
           <BookList books={books} onToggle={toggleBook} /> 
         ) : (
-          <p>Нет Книг!</p>
+          loading ? null :(
+            <p>Нет Книг!</p>
+          )
         )}
         
       </div>
---
+++ src/loader.js
@@ -0,0 +1,3 @@
+
+
+export default () => <div className='loading'><div class="lds-ripple"><div></div><div></div></div></div>
